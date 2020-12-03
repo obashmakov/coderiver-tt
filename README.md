8240e6acbc08f5a4451cf6d61fa65056cbe7ff58
Olexiy Bashmakov <olexiybashmakov@gmail.com>
2020-12-03 18:28:49 +0200

    tried to realise responsive design

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # Coderiver-TT
 
 - [Demo Link](https://obashmakov.github.io/coderiver-tt/)
+
+## Description
+
+Layout implemented according to [this design](https://drive.google.com/drive/folders/1F_mf4FpdWhG3pnm966G2IOsKZq1A4X-j)
+
+This project was realised using React(Hooks), HTML/CSS, SASS, BEM
+
+On Desktop page you can open and close Menu. Also you can change the status online by clicking on
+dropdow in Header. The buttons in chart are clickable.
+
+## Installing
+
+- Fork and clone this repository
+- Run npm install in your terminal
+- Run npm start
--- src/components/Menu/Navigation/Navigation.jsx
+++ src/components/Menu/Navigation/Navigation.jsx
@@ -9,32 +9,32 @@ export const Navigation = () => (
       </div>
       <ul className="nav__list">
         <li className="nav__item nav__item--active">
-          <a className="nav__link" href="/">
+          <a className="nav__link" href="/coderiver-tt/">
             Profit
           </a>
         </li>
         <li className="nav__item">
-          <a className="nav__link" href="/">
+          <a className="nav__link" href="/coderiver-tt/">
             Explore
           </a>
         </li>
         <li className="nav__item">
-          <a className="nav__link" href="/">
+          <a className="nav__link" href="/coderiver-tt/">
             Profile
           </a>
         </li>
         <li className="nav__item">
-          <a className="nav__link" href="/">
+          <a className="nav__link" href="/coderiver-tt/">
             Photos
           </a>
         </li>
         <li className="nav__item">
-          <a className="nav__link" href="/">
+          <a className="nav__link" href="/coderiver-tt/">
             Videos
           </a>
         </li>
         <li className="nav__item">
-          <a className="nav__link" href="/">
+          <a className="nav__link" href="/coderiver-tt/">
             Invite
             <span className="nav__invites">
               54
@@ -42,27 +42,27 @@ export const Navigation = () => (
           </a>
         </li>
         <li className="nav__item">
-          <a className="nav__link" href="/">
+          <a className="nav__link" href="/coderiver-tt/">
             Music
           </a>
         </li>
         <li className="nav__item">
-          <a className="nav__link" href="/">
+          <a className="nav__link" href="/coderiver-tt/">
             Candidates
           </a>
         </li>
         <li className="nav__item">
-          <a className="nav__link" href="/">
+          <a className="nav__link" href="/coderiver-tt/">
             Calendar
           </a>
         </li>
         <li className="nav__item">
-          <a className="nav__link" href="/">
+          <a className="nav__link" href="/coderiver-tt/">
             Feedback
           </a>
         </li>
         <li className="nav__item">
-          <a className="nav__link" href="/">
+          <a className="nav__link" href="/coderiver-tt/">
             Help
           </a>
         </li>
